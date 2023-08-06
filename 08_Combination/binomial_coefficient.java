31a559e9fa4956af33ec2c7ccbe5043d54285357
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2023-08-06 18:42:42 +0900

    .

---
+++ 08_Combination/binomial_coefficient.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+// BAEKJOON num.11050
+public class binomial_coefficient {
+    static int N, K;
+    static int[][] D;
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        D = new int[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            D[i][1] = i;
+            D[i][0] = 1;
+            D[i][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            }
+        }
+
+        System.out.println(D[N][K]);
+        sc.close();
+    }
+}
